e694ff38f81e4e59f0c5eddc5d1ffcaa5e3968d8
paola-c7 <paola.caloiero@itispaleocapa.it>
2026-05-27 16:16:22 +0200

    Add files via upload

---
+++ Client/EchoClient/EchoClient/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Net.Sockets;
+using System.Text;
+
+class Client
+{
+    static void Main()
+    {
+        TcpClient client = new TcpClient("127.0.0.1", 8080);
+        Console.WriteLine("Connesso al server!");
+
+        NetworkStream stream = client.GetStream();
+
+        // Invio messaggio
+        string message = "Ciao server!";
+        byte[] data = Encoding.UTF8.GetBytes(message);
+        stream.Write(data, 0, data.Length);
+        Console.WriteLine($"Inviato: {message}");
+
+        // Ricezione risposta
+        byte[] buffer = new byte[1024];
+        int bytesRead = stream.Read(buffer, 0, buffer.Length);
+        string reply = Encoding.UTF8.GetString(buffer, 0, bytesRead);
+        Console.WriteLine($"Risposta: {reply}");
+
+        client.Close();
+    }
+}
---
+++ Server/EchoServer/EchoServer/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Net;
+using System.Net.Sockets;
+using System.Text;
+
+class Server
+{
+    static void Main()
+    {
+        TcpListener server = new TcpListener(IPAddress.Any, 8080);
+        server.Start();
+        Console.WriteLine("Server in ascolto sulla porta 8080...");
+
+        // Attesa bloccante del client
+        TcpClient client = server.AcceptTcpClient();
+        Console.WriteLine("Client connesso!");
+
+        NetworkStream stream = client.GetStream();
+
+        // Lettura dati
+        byte[] buffer = new byte[1024];
+        int bytesRead = stream.Read(buffer, 0, buffer.Length);
+        string msg = Encoding.UTF8.GetString(buffer, 0, bytesRead);
+        Console.WriteLine($"Ricevuto: {msg}");
+
+        // Risposta (Echo)
+        byte[] reply = Encoding.UTF8.GetBytes("Echo: " + msg);
+        stream.Write(reply, 0, reply.Length);
+
+        client.Close();
+        server.Stop();
+    }
+}
